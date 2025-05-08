9e716760adbd0262d1eb14709629f0ee110e9d9e
Tim Shand <tim@tshand.com>
2025-05-08 21:16:22 +1200

    Initial commit.

---
+++ shell-startup-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script Name: shell-startup-script.sh
+# Description: Intended to be executed on terminal startup. Provides IP and general info. 
+# Author: Tim Shand
+# Date: 2025-05-08
+# Usage: ./shell-startup-script.sh (set to execute on startup in terminal settings).
+
+# Global Variables
+
+# Functions
+
+# Main
